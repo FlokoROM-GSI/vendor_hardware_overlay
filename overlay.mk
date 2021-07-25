1aaf28c3513dfe1c70b7f22ee88e67a7dea8e955
AndroPlus <hosigumayuugi@gmail.com>
2021-07-25 12:58:28 +0900

    Add overlay for Xiaomi Mi 11 Lite 5G

--- overlay.mk
+++ overlay.mk
@@ -144,6 +144,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-mi8se \
 	treble-overlay-xiaomi-mi9 \
 	treble-overlay-xiaomi-mi9se \
+	treble-overlay-xiaomi-mi11lite5g \
 	treble-overlay-xiaomi-mia2 \
 	treble-overlay-xiaomi-mia2lite \
 	treble-overlay-xiaomi-mimax3 \
